bd8f6d1323633c0ac0e34920868abaa220964e8e
Albert <a.bonetvalle@gmail.com>
2023-06-23 18:26:33 +0200

    App import modified cuz modularization.

--- app/src/main/java/com/albertbonet/pokeapp/App.kt
+++ app/src/main/java/com/albertbonet/pokeapp/App.kt
@@ -2,7 +2,7 @@ package com.albertbonet.pokeapp
 
 import android.app.Application
 import androidx.room.Room
-import com.albertbonet.pokeapp.framework.database.PokemonDatabase
+import com.albertbonet.pokeapp.data.database.PokemonDatabase
 
 class App: Application() {
 
